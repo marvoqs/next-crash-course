fe022c46f5d2ee49bda531c90a9c093dd0fb68b1
Marek Schwarz <marek.schwarz@designdev.cz>
2022-10-13 21:46:46 +0200

    Add article list, fetch articles

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 
-export default function Home() {
+import ArticleList from "../components/ArticleList";
+
+export default function Home({ articles }) {
   return (
     <div>
       <Head>
@@ -8,7 +10,20 @@ export default function Home() {
         <meta name="keywords" conent="web development, programming" />
       </Head>
 
-      <h1>Welcome to Next</h1>
+      <ArticleList articles={articles} />
     </div>
   );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=6`
+  );
+  const articles = await res.json();
+
+  return {
+    props: {
+      articles,
+    },
+  };
+};
